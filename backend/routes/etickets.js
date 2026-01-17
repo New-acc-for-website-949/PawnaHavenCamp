80e4417ebb3328490e782d85dbdceb1d54aed39b
New-acc-for-website-949 <hrushikeshmore949@gmail.com>
2026-01-17 14:32:12 +0530

    Delete backend directory

--- backend/routes/etickets.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eticketController = require('../controllers/eticketController');
-
-router.post('/', eticketController.createETicket);
-router.get('/:ticketId', eticketController.getETicketById);
-
-module.exports = router;
